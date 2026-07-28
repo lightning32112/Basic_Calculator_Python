1b934c53592fee8ddb57ee17a8364690f5d39b16
Hashim Zahid <hashi.zee12@gmail.com>
2026-07-28 15:40:18 +0100

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,8 @@
+#adding a loop so that if the user wants to they can use the calculator again
+while true:
+  function = input("please enter one of the following: +, -, * or /")
+  num1 = float(input("choose your first number: ")
+  num2 = float(input("choose your second number: ")
+
+  print (funtion, num1, num2)
+  
